1679268b222af0e934e83634c032c5b9078a31c0
Chinmay1635 <chinmaykulkarni165@gmail.com>
2025-04-20 08:37:33 +0530

    optimized account card

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -30,7 +30,7 @@ export function AccountCard({ account }) {
     event.preventDefault(); // Prevent navigation
 
     if (isDefault) {
-      toast.warning("You need atleast 1 default account");
+      toast.warning("You need at least 1 default account");
       return; // Don't allow toggling off the default account
     }
 
@@ -50,33 +50,39 @@ export function AccountCard({ account }) {
   }, [error]);
 
   return (
-    <Card className="hover:shadow-md transition-shadow group relative">
+    <Card className="bg-gray-900/80 backdrop-blur-lg  border-sky-200 border-s hover:shadow-lg hover:shadow-yellow-500/10 transition-all group relative">
       <Link href={`/account/${id}`}>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium capitalize">
+          <CardTitle className="text-sm font-medium capitalize text-gray-300">
             {name}
+            {isDefault && (
+              <Badge className="ml-2 bg-sky-500/20 text-sky-400 hover:bg-sky-700/30">
+                Default
+              </Badge>
+            )}
           </CardTitle>
           <Switch
             checked={isDefault}
             onClick={handleDefaultChange}
             disabled={updateDefaultLoading}
+            className="data-[state=checked]:bg-sky-300 data-[state=unchecked]:bg-gray-700"
           />
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">
+          <div className="text-2xl font-bold text-white">
             ₹{parseFloat(balance).toFixed(2)}
           </div>
-          <p className="text-xs text-muted-foreground">
+          <p className="text-xs text-gray-400">
             {type.charAt(0) + type.slice(1).toLowerCase()} Account
           </p>
         </CardContent>
-        <CardFooter className="flex justify-between text-sm text-muted-foreground">
-          <div className="flex items-center">
-            <ArrowUpRight className="mr-1 h-4 w-4 text-green-500" />
+        <CardFooter className="flex justify-between text-sm">
+          <div className="flex items-center text-green-400">
+            <ArrowUpRight className="mr-1 h-4 w-4" />
             Income
           </div>
-          <div className="flex items-center">
-            <ArrowDownRight className="mr-1 h-4 w-4 text-red-500" />
+          <div className="flex items-center text-red-400">
+            <ArrowDownRight className="mr-1 h-4 w-4" />
             Expense
           </div>
         </CardFooter>
